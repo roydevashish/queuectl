3495c5aa04fd799a03c2682397d7a431889231d3
Devashish Roy <medevashishroy@gmail.com>
2025-11-06 13:50:56 +0530

    feat: add status
    need to fetch active workers

--- internal/cli/status.go
+++ internal/cli/status.go
@@ -1,7 +1,12 @@
 package cli
 
 import (
+	"os"
+	"strconv"
+
+	"github.com/aquasecurity/table"
 	clilogger "github.com/roydevashish/queuectl/internal/cli_logger"
+	"github.com/roydevashish/queuectl/internal/storage"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +17,25 @@ var StatusCmd = &cobra.Command{
 (pending, running, completed, failed, dead) and number of workers.`,
 
 	Run: func(cmd *cobra.Command, args []string) {
-		clilogger.LogSuccess("show system status")
+		rows, _ := storage.DB.Query(`SELECT state, COUNT(*) FROM jobs GROUP BY state`)
+		defer rows.Close()
+		counts := map[string]int{}
+		for rows.Next() {
+			var state string
+			var count int
+			rows.Scan(&state, &count)
+			counts[state] = count
+		}
+
+		// need to fetch active workers
+		activeWorkers := strconv.Itoa(4)
+
+		clilogger.LogInfo("status")
+		t := table.New(os.Stdout)
+		t.SetAlignment(table.AlignCenter, table.AlignCenter, table.AlignCenter, table.AlignCenter, table.AlignCenter, table.AlignCenter)
+
+		t.SetHeaders("💻 workers", "⏳ pending", "🔄 processing", "✅ completed", "❌ failed", "💀 dead")
+		t.AddRow(activeWorkers, strconv.Itoa(counts["pending"]), strconv.Itoa(counts["processing"]), strconv.Itoa(counts["completed"]), strconv.Itoa(counts["failed"]), strconv.Itoa(counts["dead"]))
+		t.Render()
 	},
 }
